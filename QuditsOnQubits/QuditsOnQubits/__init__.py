25fcd5d47989c9e3fbaeec6582f443c3b036668a
slysek <szymonlysek123@gmail.com>
2025-07-19 12:43:40 +0200

    2qutrit, ghz and ame43 working and real measurement on IBMQ computers

--- QuditsOnQubits/QuditsOnQubits/__init__.py
+++ QuditsOnQubits/QuditsOnQubits/__init__.py
@@ -2,11 +2,11 @@ from .quditsonqubits import QuditsOnQubits
 
 from .ghz_creator import create_ghz, create_graph_ghz, get_f_gate
 from .ame_creator import create_ame43
-from .brisbane import brisbane
-from .torino import torino
-from .distrib import distrib
-from .compare_distrib import compare_distrb
-from .fidelity import fidelity
-from .bell_operator import bell_operator
+#from .brisbane import brisbane
+#from .torino import torino
+#from .distrib import distrib
+#from .compare_distrib import compare_distrb
+#from .fidelity import fidelity
+#from .bell_operator import bell_operator
 # from .bell2 import bell_operator
 # from .bell3 import bell_operator
